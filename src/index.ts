d9274e924781a3debaca0cf70d912652033b96d4
jkvyff <kristianvyff@gmail.com>
2019-11-07 16:30:09 -0800

    Skeleton added

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { UserResolver } from './UserResolver';
+import { createConnection } from 'typeorm';
+
+
+(async() => {
+    const app = express()
+    app.get('/', (_req, res) => res.send('hello'));
+
+    await createConnection();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver]
+        })
+    })
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("express server started")
+    })
+})()
